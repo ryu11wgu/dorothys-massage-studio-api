a7dde1b5acdc81481a5713cd775198c6bcabda52
Raymond <ryu11wgu@gmail.com>
2026-04-18 16:49:11 -0700

    Initial Spring Boot backend setup

---
+++ src/main/java/com/raymond/dorothysmassagestudio/DorothysMassageStudioApiApplication.java
@@ -0,0 +1,13 @@
+package com.raymond.dorothysmassagestudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DorothysMassageStudioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DorothysMassageStudioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raymond/dorothysmassagestudio/DorothysMassageStudioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raymond.dorothysmassagestudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DorothysMassageStudioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
