d931bedc8461d09de4ee517616d1c1cd1fbb29e7
Raymond <ryu11wgu@gmail.com>
2026-04-29 15:32:06 -0700

    Add BusinessInfo REST controller

---
+++ src/main/java/com/raymond/dorothysmassagestudio/controller/BusinessInfoController.java
@@ -0,0 +1,21 @@
+package com.raymond.dorothysmassagestudio.controller;
+
+import com.raymond.dorothysmassagestudio.dto.BusinessInfoResponse;
+import com.raymond.dorothysmassagestudio.service.BusinessInfoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BusinessInfoController {
+
+    private final BusinessInfoService businessInfoService;
+
+    public BusinessInfoController(BusinessInfoService businessInfoService) {
+        this.businessInfoService = businessInfoService;
+    }
+
+    @GetMapping("/api/business-info")
+    public BusinessInfoResponse getBusinessInfo() {
+        return businessInfoService.getBusinessInfo();
+    }
+}
